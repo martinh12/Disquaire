651e3e031a13bca4bb52d47b4473a0ca7f3c609b
Martin Hamon <martinh@outlook.fr>
2019-08-12 16:20:05 +0200

    création du projet

---
+++ disquaire-back/app.js
@@ -0,0 +1,36 @@
+const cookieParser = require('cookie-parser');
+const express = require('express');
+const httpErrors = require('http-errors');
+const logger = require('morgan');
+const path = require('path');
+
+const indexRouter = require('./routes/index');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(httpErrors(404));
+});
+
+// error handler
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.json(err);
+});
+
+module.exports = app;
---
+++ disquaire-front/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import { NotificationContainer } from 'react-notifications';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <Switch>
+
+      </Switch>
+      <NotificationContainer />
+    </div>
+  );
+}
+
+export default App;
